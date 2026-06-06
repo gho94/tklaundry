f4bb407dec84b81d97360b076a980f62185f7963
gho94 <yorjsgh@naver.com>
2026-06-07 07:12:37 +0900

    chore : Java 프로젝트 추가

---
+++ backend/tklaundry-api/src/main/java/com/tklaundry/tklaundry_api/TklaundryApiApplication.java
@@ -0,0 +1,13 @@
+package com.tklaundry.tklaundry_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TklaundryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TklaundryApiApplication.class, args);
+	}
+
+}
---
+++ backend/tklaundry-api/src/test/java/com/tklaundry/tklaundry_api/TklaundryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tklaundry.tklaundry_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TklaundryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
