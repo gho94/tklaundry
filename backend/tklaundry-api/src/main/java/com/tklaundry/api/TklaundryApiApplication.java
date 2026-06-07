e4d3cf27ee7f0356a054892859781145c5dc86d5
gho94 <yorjsgh@naver.com>
2026-06-07 20:03:49 +0900

    chore : Java 패키지 경로 변경 및 테스트 코드 삭제

--- backend/tklaundry-api/src/main/java/com/tklaundry/api/TklaundryApiApplication.java
+++ backend/tklaundry-api/src/main/java/com/tklaundry/api/TklaundryApiApplication.java
@@ -1,4 +1,4 @@
-package com.tklaundry.tklaundry_api;
+package com.tklaundry.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
